e2c8a6585f26ef70cdaf1dce825989b8ead4bd16
Tianyi-W <tianyiwu@amazon.com>
2022-07-26 16:37:56 -0700

    test dynamicComment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import { GitHub } from '@actions/github/lib/utils';
+//import { GitHub } from '@actions/github/lib/utils';
 import { GithubApi } from './github';
 
 async function run() {
@@ -53,7 +53,7 @@ async function run() {
     // place to generste dynamic comments
     // it has user_name, labels and label meanings
     const dynamicComments = 'welcome' + github.getIssueCreator() + ', the CDK Team thanks you for being a' + setLabels + 'to the CDK. This means that you have made' + determineMeaning(meanings, index);
-
+    console.log(dynamicComments);
     if (dynamicComments !== undefined) {
       await github.writePRComments(dynamicComments).catch(error => {core.setFailed(error.message);});
     }
