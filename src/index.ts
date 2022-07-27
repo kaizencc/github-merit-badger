e4f035e03b5873d4dc6d998884306b1c9b363691
Parker Scanlon <pscanlo@amazon.com>
2022-07-27 15:06:05 -0700

    use category to separate flow

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function run() {
   const labelsRaw: string = core.getInput('labels');
   const bucketsRaw: string = core.getInput('buckets');
   const meaningsRaw: string = core.getInput('label-meanings');
-  //const category: string = core.getInput('category');
+  const category: string = core.getInput('category');
 
   //console.log(labelsRaw);
   //console.log(bucketsRaw);
@@ -29,7 +29,7 @@ async function run() {
   //console.log(labels);
   //console.log(buckets);
   //console.log(category);
-  console.log(meanings);
+  //console.log(meanings);
 
   //const dummyVal = 0;
   //const setLabels = determinLabels(labels, buckets, dummyVal);
@@ -37,57 +37,65 @@ async function run() {
   const github: GithubApi = new GithubApi(token);
   //console.log(github.getIssueNumber());
 
-  const numPRs = await github.paginateData();
-  console.log(numPRs);
-  if (numPRs !== undefined) {
+
+  if (category === 'total') {
+    const numPRs = await github.paginateData(); // TODO: change from old behavior to new
     console.log(numPRs);
+    if (numPRs !== undefined) {
+      console.log(numPRs);
 
-    const index = determineIndex(buckets, numPRs);
-    const setLabels = determinLabel(labels, index);
+      const index = determineIndex(buckets, numPRs);
+      const setLabels = determinLabel(labels, index);
 
-    if (setLabels != []) {
-      console.log(setLabels);
-      await github.setPullRequestLabels(setLabels).catch(error => {core.setFailed(error.message);}); // .catch(error => {core.setFailed(error.message);}); ?
+      if (setLabels != []) {
+        console.log(setLabels);
+        await github.setPullRequestLabels(setLabels).catch(error => {core.setFailed(error.message);}); // .catch(error => {core.setFailed(error.message);}); ?
+      }
+
+      // place to generste dynamic comments
+      // it has user_name, labels and label meanings
+      const dynamicComments = 'welcome ' + await github.getIssueCreator().catch(error => {core.setFailed(error.message);}) + ', the CDK Team thanks you for being a ' + setLabels + ' to the CDK. This means that you have made ' + determineMeaning(meanings, index);
+      //console.log(dynamicComments);
+      if (dynamicComments !== undefined) {
+        await github.writePRComments(dynamicComments).catch(error => {core.setFailed(error.message);});
+      }
     }
 
-    // place to generste dynamic comments
-    // it has user_name, labels and label meanings
-    const dynamicComments = 'welcome ' + await github.getIssueCreator().catch(error => {core.setFailed(error.message);}) + ', the CDK Team thanks you for being a ' + setLabels + ' to the CDK. This means that you have made ' + determineMeaning(meanings, index);
-    //console.log(dynamicComments);
-    if (dynamicComments !== undefined) {
-      await github.writePRComments(dynamicComments).catch(error => {core.setFailed(error.message);});
-    }
+
+    //const data = await github.getPullsData().catch(error => {
+    //  core.setFailed(error.message);
+    //});
+
+    //console.log(data);
+
+    //if (data !== undefined) { // if (result instanceof Object) {
+    //  for (let i = 0; i < data.length; i += 1) {
+    //    console.log(data[i].user?.login);
+    //  }
+    //}
+
+    //console.log('\nReset\n');
+
+    /*const creator = await github.getIssueCreator().catch(error => {
+      core.setFailed(error.message);
+    });
+    */
+
+    //if (creator !== undefined) {
+    //await github.paginateData().catch(error => {
+    //  core.setFailed(error.message);
+    //});
+    //}
+
+    //console.log(github.context.issue.owner);
+    //const content = github.
+  } else if (category === 'hotlist') {
+    // call different functions from github
+    // alternatively, can determine labels + comment and do setting in 1 step
+    // also consider editing comment as a strech goal
+  } else {
+    // do nothing
   }
-
-
-  //const data = await github.getPullsData().catch(error => {
-  //  core.setFailed(error.message);
-  //});
-
-  //console.log(data);
-
-  //if (data !== undefined) { // if (result instanceof Object) {
-  //  for (let i = 0; i < data.length; i += 1) {
-  //    console.log(data[i].user?.login);
-  //  }
-  //}
-
-  //console.log('\nReset\n');
-
-  /*const creator = await github.getIssueCreator().catch(error => {
-    core.setFailed(error.message);
-  });
-  */
-
-  //if (creator !== undefined) {
-  //await github.paginateData().catch(error => {
-  //  core.setFailed(error.message);
-  //});
-  //}
-
-  //console.log(github.context.issue.owner);
-  //const content = github.
-
 }
 
 run().catch(error => {
